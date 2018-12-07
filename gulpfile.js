137022a20b06bd772482ab507ae846120bbc26d0
ixdu <ix@2du.ru>
2018-12-08 03:34:05 +0500

    implemented log showing feature from sqltoollog table

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,24 @@ var mysql_config = {
     multipleStatements: true
 };
 
+var lastMessage = '';
+setInterval(function(){
+    var connection = mysql.createConnection(mysql_config);
+    connection.connect();
+
+    connection.query('select * from sqltoollog', function (error, results, fields) {
+	if(lastMessage == results[0].message)
+	    return;
+	
+	if(error)
+	    console.log(error.sqlMessage);
+	else
+	    console.log(results[0].message);
+	lastMessage = results[0].message;
+	connection.end();    
+    });    
+}, 3000);
+
 gulp.task('default', [], function(arg){
     gulp.watch(pathToSqlDirectory + '**/**/*.sql', function(event){
 	var connection = mysql.createConnection(mysql_config);
